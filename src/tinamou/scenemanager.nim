6ab8bc2224987e77b7501cf033a57d06f08155e4
Double-oxygeN <mail@double-oxygen.net>
2018-07-04 15:44:24 +0900

    :wrench: add docstring

--- src/tinamou/scenemanager.nim
+++ src/tinamou/scenemanager.nim
@@ -11,14 +11,18 @@ type
     table: TableRef[SceneId, BaseScene]
 
 proc newSceneManager*: SceneManager =
+  ## Create new SceneManager.
   new result
   result.table = newTable[string, BaseScene]()
 
 proc setScene*(self: SceneManager, name: string, scene: BaseScene) =
+  ## Set scene with key string.
   if not self.table.hasKey(name):
     self.table.add(name, scene)
 
 proc getScene*(self: SceneManager, name: string): BaseScene =
+  ## Get scene by key string.
+  ## If no scenes are found, then throw exception.
   try:
     result = self.table[name]
   except KeyError:
